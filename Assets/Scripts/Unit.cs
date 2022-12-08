11a6a4413a1c6fedf99d553bdff4d40bd57de70b
imohori0826 <imoohori0826@gmail.com>
2022-12-08 23:33:06 +0900

    UI完成

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Unit : MonoBehaviour
 {
+    public Slider HPSlider;
+    public int hp;
+    int hpMax = 10;
+    public int at;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hp = hpMax;
+        HPSlider.maxValue = hpMax;
+        HPSlider.value = hpMax;
+        at = 10;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDamage(int _damage)
     {
-        
+        hp -= _damage;
+        if (hp <= 0)
+        {
+            hp = 0;
+        }
+        HPSlider.value = hp;
     }
 }
